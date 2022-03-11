34dda7e059b1b14611e7e72578c051fa4a5921b3
Agnes <51322389+yuchen0531@users.noreply.github.com>
2022-03-11 17:12:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <!-- <link rel="ico" type="image/x-icon" href="static/favicon.ico"> -->
+  <link rel="shortcut icon" type="image/x-icon" href="/assets/newcomer/static/favicon.ico">   
+    <title>點數代管_商家後台</title>
+    <!-- 需修改 -->
+    <meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta NAME="KeyWords" CONTENT="uupon,APP,悠遊卡,點數,紅利點數,集點,折抵電信費,悠遊卡集點,會員點數,點數交換,好康">
+    <meta NAME="Description" CONTENT="下載UUPON APP，加入會員開啟悠遊卡集點，執行賺點任務，點數還可以換超商咖啡！">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="company" content="UUPON.inc 點鑽整合行銷股份有限公司">
+    <meta itemprop="image" content="/assets/pointDemo/static/img/ogImg.8e723e1.png"/>
+    <meta name="copyright" content="版權所有 Copyright © 2020 UUPON Inc. All Rights Reserved.">
+    <meta property="og:title" content="UUPON">
+    <meta property="og:description" content="Hi ！UUPON新手任務上線囉！ 讓你30點快速賺！趕快下載UUPON  APP加入會員，一起玩任務賺點數！">
+    <meta property="og:image" content="/assets/pointDemo/static/img/ogImg.8e723e1.png"/>
+    <!-- END -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js" integrity="sha512-M+qMI1PHRcYcOpJzeJlaWbVVx2JJyPIwZas8or7dc97LZOokjvbpfRxymhVtlJLyjiF3wGyr0FJOA4DLONLVLw==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58071296-17"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-58071296-17');
+    </script>
+  <link href="/assets/pointDemo/static/css/app-app20211119-c.css" rel="stylesheet"></head>
+    <body>
+    <div id="app">
+      <input type="hidden" name="ut" value="<?=$ut ?>">
+    </div>
+    <!-- built files will be auto injected -->
+  <script type="text/javascript" src="/assets/pointDemo/static/js/manifest-app20211119-c.js"></script><script type="text/javascript" src="/assets/pointDemo/static/js/vendor-app20211119-c.js"></script><script type="text/javascript" src="/assets/pointDemo/static/js/app-app20211119-c.js"></script></body>
+</html>
